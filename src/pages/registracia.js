33d667cec5402d7d8158f1229fbc79457a10600f
simon-ugor <simonugor@MacBook-Pro-uzivatela-Simon.local>
2023-06-18 03:57:11 +0200

    post api

--- src/pages/registracia.js
+++ src/pages/registracia.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 
 const registracia = () => {
+
+    const [email, setEmail] = useState("");
+    const [placeHolder, setPlaceHolder] = useState("E-mail");
+
+    const submitClick = () => {
+        if (email.includes("@") == true && email.includes(".") && email.length < 320) {
+            postApi();
+        } else {
+            //throw an error
+            setPlaceHolder("Neplatný formát");
+            setEmail("");
+        }
+    }
+
+    const postApi = async () => {
+        const res = await fetch("https://648e5e272de8d0ea11e8adf8.mockapi.io/emails", {
+            method: "POST",
+            body: JSON.stringify({"email": email}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await res.json();
+        setPlaceHolder("E-mail úspešne odoslaný");
+        setEmail("");
+        return data;
+    }
+
   return (
     <>
         <Head>
@@ -19,9 +47,9 @@ const registracia = () => {
                 <div></div>
                 <div className='pages-text-div'>
                     <p className='pages-text'>Zadaj svoj e-mail</p>
-                    <input placeholder='E-mail' className='email-input'></input>
+                    <input placeholder={placeHolder} className='email-input' value={email} onChange={(e) => setEmail(e.currentTarget.value)}></input>
                     <br />
-                    <button className='submit-email-button'>Odoslať</button>
+                    <button className='submit-email-button' onClick={submitClick}>Odoslať</button>
                 </div>
                 <div></div>
             </div>
